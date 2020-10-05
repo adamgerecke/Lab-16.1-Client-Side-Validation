a0bc728bdaba0bb35eedab0a228eb329cb14fa5d
Adam Gerecke <adamgerecke@quickenloans.com>
2020-10-05 18:41:02 -0400

    MVP Finished

---
+++ Lab_16.1_Client-Side Validation/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Lab_16._1_Client_Side_Validation.Models;
+
+namespace Lab_16._1_Client_Side_Validation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        
+        public IActionResult Result(string FirstName, string LastName, string Email)
+        {
+            return Content($"Thank you for registering {FirstName} {LastName}. Any future communication will be sent to you at {Email}");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab_16.1_Client-Side Validation/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_16._1_Client_Side_Validation.Models
+{
+    public class User
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+        public string password;
+
+    }
+}
